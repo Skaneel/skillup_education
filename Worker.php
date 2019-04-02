16d872cf45b878335d4faac860b12be2bd170300
aleksandr <fitline.in.ua@gmail.com>
2019-04-02 15:57:16 +0300

    1-3 tasks

---
+++ Worker.php
@@ -0,0 +1,73 @@
+<?php
+
+
+class Worker
+{
+    private $temp;
+//    public $name;
+    private $name;
+
+//    public $age;
+    private $age;
+
+//    public $salary;
+    private $salary;
+
+    function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function setAge($age)
+    {
+        $this->checkAge($age);
+
+    }
+
+    function getAge()
+    {
+        return $this->age;
+    }
+
+    function setSalary($salary)
+    {
+        $this->salary = $salary;
+    }
+
+    function getSalary()
+    {
+        return $this->salary;
+    }
+
+    private function checkAge($age)
+    {
+        if ($age >= 1 && $age <= 100)
+            $this->age = $age;
+    }
+
+}
+
+$obj1 = new Worker();
+$obj2 = new Worker();
+
+//$obj1->name = 'Иван';
+//$obj1->age = '26';
+//$obj1->salary = '2000';
+
+$obj1->setAge(18);
+$obj1->setName('Иван');
+$obj1->setSalary(30000);
+
+$obj1->setAge(50);
+
+echo $obj1->getAge() . '<br>';
+echo $obj1->getName() . '<br>';
+echo $obj1->getSalary();
+
+
+
